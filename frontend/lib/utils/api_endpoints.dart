89239dc4b539611a9a922ccfd68dcd48b531e19a
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-07-29 16:22:11 +0200

    added an api_endpoint into utils

---
+++ frontend/lib/utils/api_endpoints.dart
@@ -0,0 +1,9 @@
+const String backendUrl = 'http://localhost:5001';
+const String registerEndpoint = '$backendUrl/auth/register';
+const String loginEndpoint = '$backendUrl/auth/login';
+const String logoutEndpoint = '$backendUrl/auth/logout';
+
+const String addToWatchlistEndpoint = '$backendUrl/watchlist';
+
+String deleteFromWatchlistEndpoint(String id) => '$backendUrl/watchlist/$id';
+String updateWatchlistEndpoint(String id) => '$backendUrl/watchlist/$id';
