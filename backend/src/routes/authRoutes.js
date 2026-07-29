184aef158698715b350ca25b5d2eb6983d67000d
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-07-29 18:08:08 +0200

    added route for auth/me

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"; 
-import {register, login, logout} from '../controllers/authController.js';
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import {register, login, logout, me} from '../controllers/authController.js';
 import { validateRequest } from "../middleware/validateRequestMiddleware.js";
 import { loginSchema, registerSchema } from "../validators/authValidators.js"; 
 const router = express.Router(); 
@@ -9,6 +10,7 @@ router.post("/register", validateRequest(registerSchema), register);
 router.post("/login", validateRequest(loginSchema),login);
 
 router.post("/logout", logout);
+router.get("/me", authMiddleware, me); 
 
 export default router; 
 
