579cedb2c1ee8f2a650d42bc1f5469164190899f
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-07-29 18:18:55 +0200

    added loading for 2 seconds as well as loading text

--- frontend/lib/pages/auth_pages/auth_wrapper.dart
+++ frontend/lib/pages/auth_pages/auth_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/pages/auth_pages/login_page.dart';
 import 'package:frontend/pages/navigation/main_navigation.dart';
 import 'package:frontend/providers/auth_provider.dart';
+import 'package:frontend/themes/text_styles.dart';
 import 'package:provider/provider.dart';
 
 class AuthWrapper extends StatefulWidget {
@@ -22,7 +23,10 @@ class _AuthWrapperState extends State<AuthWrapper> {
 
   Future<void> _checkAuth() async {
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    await authProvider.tryAutoLogin();
+    await Future.wait([
+      Future.delayed(const Duration(seconds: 2)),
+      authProvider.tryAutoLogin(),
+    ]);
 
     setState(() {
       _checkingAuth = false;
@@ -32,7 +36,22 @@ class _AuthWrapperState extends State<AuthWrapper> {
   @override
   Widget build(BuildContext context) {
     if (_checkingAuth) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+      return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image.network(
+              //   "https://user-images.githubusercontent.com/6876788/96633009-d1818000-1318-11eb-9f1d-7f914f4ccb16.gif",
+              // ),
+              Text("Loading", style: AppTextStyles.sectionTitle),
+              const SizedBox(height: 32),
+
+              CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      );
     }
 
     final authProvider = Provider.of<AuthProvider>(context);
