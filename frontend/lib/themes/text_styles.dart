06dcda45a46bbbd2f161de916fb564b2ddc8309d
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-07-29 13:01:44 +0200

    added textstyles into project

--- frontend/lib/themes/text_styles.dart
+++ frontend/lib/themes/text_styles.dart
@@ -1,2 +1,54 @@
 // Configure text styles
-class AppTextStyles {}
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:frontend/themes/colors.dart';
+
+class AppTextStyles {
+  static const TextStyle pageTitle = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+  );
+
+  static const TextStyle cardTitle = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  static const TextStyle categoryText = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+
+  static const TextStyle buttonTextBlack = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static const TextStyle buttonTextWhite = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static const TextStyle navbarText = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle introTitle = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 38,
+  );
+
+  static const TextStyle sectionTitle = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+}
